4749d6ca8cb36e199ef49b4fa720686fde6f6e51
Mehedi Gazi <71560571+mehedi-gazi@users.noreply.github.com>
2025-03-27 23:28:28 +0000

    27.3.25

--- testing-ground.c
+++ testing-ground.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 
 int main() {
-  int arr[] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 22, 26, 28, 29};
-  int arr2[100];
-    
-  // Code for Checkpoint 1 goes here
-  for (int i = 0; i < 20; i++) {
+  double arr[] = {1.0, 8, 2.27, 16.2, 2, 3.14, 12.6, 1.62, 6.54, 9, 11};
+  
+  // Checkpoint 1 code goes here.
+  int len = sizeof(arr)/sizeof(double);
+  printf("%i\n", len);
+
+  // Checkpoint 2 code goes here.
+  for (int i = 0; i < len; i++) {
     printf("%i\n", arr[i]);
   }
-    
-  // Code for Checkpoint 2 goes here
 }
