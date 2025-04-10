38f08611cb924d605f7afbef9a934ba441cd267d
Mehedi Gazi <71560571+mehedi-gazi@users.noreply.github.com>
2025-04-10 16:39:45 +0100

    Project 7 - Mini Calendar Complete - Most challenging one yet.

--- Projects/project7-minicalendar.c
+++ Projects/project7-minicalendar.c
@@ -31,47 +31,55 @@ int days_in_month[] = {0 , // Placeholder - Index 0
      31, // December -- Index 12
      '\0'};
 
-void add_days_to_date(int* dd, int* mm, int* yr, int days_left_to_add) { // Pointers so we can modify the original value
-    *dd = *dd + days_left_to_add;
-    *mm = *mm + days_left_to_add;
-    *yr = *yr + days_left_to_add;
-
-    int days_left_in_month;
-
-    while (days_left_to_add > 0) {
-        days_left_in_month = days_in_month[*mm] - *dd;
-
-        if (days_in_month == *mm && *yr == is_leap_year) {
-            days_left_in_month += 1;
+     void add_days_to_date(int* dd, int* mm, int* yr, int days_left_to_add) {
+        int days_left_in_month;
+    
+        while (days_left_to_add > 0) {
+            days_left_in_month = days_in_month[*mm] - *dd;
+    
+            if (*mm == 2 && is_leap_year(*yr)) { // For February if its a leap year
+                days_left_in_month += 1;
+            }
+    
+            if (days_left_to_add > days_left_in_month) { // Month and Day Correction System.
+                days_left_to_add -= (days_left_in_month + 1);
+                *dd = 1;
+    
+                if (*mm == 12) {
+                    *mm = 1;
+                    *yr += 1;
+                } else {
+                    *mm += 1;
+                }
+            } else {
+                *dd += days_left_to_add;
+                days_left_to_add = 0;
+            }
         }
     }
-
-    if (days_left_to_add > days_left_in_month) {
-        days_left_in_month - days_left_to_add;
-        days_left_to_add - 1;
-        *dd = 1;
-
-        if (*mm == 12) {
-            *mm =1;
-            *yr = *yr +1;
-        }
-        else *mm = *mm + 1;
-    }
-
-}
-
+    
 // Main Script
 int main() {
-    int year;
+    int dd, mm, yy, days_left_to_add;
 
-    printf("Enter year to check if its a leap year (1800 - 10000):");
-    scanf("%d", &year);
+    printf("Please enter a date between the years 1800 and 1000 in the format dd mm yy and provide the number of days to add to this date:");
+    scanf("%d %d %d %d", &dd, &mm, &yy, &days_left_to_add);
 
-    if (is_leap_year(year) == true) {
+    add_days_to_date(&dd, &mm, &yy, days_left_to_add);
+    printf("%d %d %d\n", dd, mm, yy);
+
+    
+    // int year;
+    // printf("Enter year to check if its a leap year (1800 - 10000):");
+    //scanf("%d", &year);
+
+    /*if (is_leap_year(year) == true) {
         printf("Leap Year, met conditions\n");
     } else {
         printf("Not Leap Year, not met conditions\n");
     }
+    */
+   return 0;
 }
 
 
