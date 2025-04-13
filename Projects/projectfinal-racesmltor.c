4f17b5aac48b2f219e5e4982b0462d4df1eeb86b
Mehedi Gazi <71560571+mehedi-gazi@users.noreply.github.com>
2025-04-13 23:13:57 +0100

    13.4 - Completed C

--- Projects/projectfinal-racesmltor.c
+++ Projects/projectfinal-racesmltor.c
@@ -1,64 +1,92 @@
-// Project VIII - Final - Race Simulator (Structures, Functions ; All relevant knowledge in C)
-
-// Inc. Libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-// Structures Sect
-struct Race {
+#include <string.h>
+// Structures section
+struct Race
+{
     int numberOfLaps;
     int currentLap;
-    char* firstPlaceDriverName;
-    char* firstPlaceRaceCarColor;
+    char firstPlaceDriverName[60];
+    char firstPlaceDriverColor[30];
 };
 
-struct RaceCar {
-    char* driverName;
-    char* raceCarColor;
+struct RaceCar
+{
+    char driverName[60];
+    char raceCarColor[30];
     int totalLapTime;
 };
-// Functions Prototypes Sect
-// Print Functions Sect
-void printIntro() {
-    printf("Welcome to our main event digital race fans!\nI hope everybody has their snacks because we are about to begin!");
+// Print functions section
+void printIntro()
+{
+    printf("Welcome to our main event digital race fans!\nI hope everybody has their snacks because we are about to begin!\n");
 }
-
-void printCountDown() {
-    printf("Racer's Ready! In...\n");
-    printf("5,\n4,\n3,\n2,\n1,\nRace!\n");
-};
-
-void printFirstPlaceAfterLap(struct Race race) {
-    printf("After lap no. %d\n", race.numberOfLaps);
-    printf("First Place -> %s in the %s race car!", race.firstPlaceDriverName, race.firstPlaceRaceCarColor);
+void printCountDown()
+{
+    printf("Racers Ready! In...\n");
+    for (int i = 5; i > 0; i--)
+    {
+        printf("%i\n", i);
+    }
+    printf("Race!\n");
 }
-
-void printCongratulation(struct Race race) {
-    printf("Let's all congratulate %s in the %s race car for the extroadinary thrilling event.\n", race.firstPlaceDriverName, race.firstPlaceRaceCarColor);
-    printf("It truly was an amazing race and thank you for attending this experience!");
+void printFirstPlaceAfterLap(struct Race race)
+{
+    printf("After lap number %d\n", race.currentLap);
+    printf("First Place is: %s in the %s race car!\n", race.firstPlaceDriverName, race.firstPlaceDriverColor);
 }
-// Logic Functions Sect
-int calculateTimeToCompleteLap () {
+void printCongratulation(struct Race race)
+{
+    printf("Let's all congratulate %s in the %s race car for an amazing performance.\n", race.firstPlaceDriverName, race.firstPlaceDriverColor);
+    printf("It truly was a great race and everybody have a goodnight!\n");
+}
+int calculateTimeToCompleteLap()
+{
     int speed = (rand() % 3) + 1;
     int acceleration = (rand() % 3) + 1;
     int nerves = (rand() % 3) + 1;
-
     return speed + acceleration + nerves;
-    
 }
-
-void updateRaceCar(struct RaceCar* raceCar) {
-    int lapTime = calculateTimeToCompleteLap();
-    raceCar -> totalLapTime += lapTime;
-};
-
-// Main Script
-int main(void) {
+// Logic functions section
+void updateRaceCar(struct RaceCar *raceCar)
+{
+    raceCar->totalLapTime += calculateTimeToCompleteLap();
+}
+void updateFirstPlace(struct Race *race, struct RaceCar *raceCar1, struct RaceCar *raceCar2)
+{
+    // checks to see who should be in first and allocates that to the pointers firstPlaceDriverName and firstPlaceDriverColor
+    if (raceCar1->totalLapTime <= raceCar2->totalLapTime)
+    {
+        strcpy(race->firstPlaceDriverName, raceCar1->driverName);
+        strcpy(race->firstPlaceDriverColor, raceCar1->raceCarColor);
+    }
+    else
+    {
+        strcpy(race->firstPlaceDriverName, raceCar2->driverName);
+        strcpy(race->firstPlaceDriverColor, raceCar2->raceCarColor);
+    }
+}
+void startRace(struct RaceCar *raceCar1, struct RaceCar *raceCar2)
+{
+    struct Race race = {5, 0, "", ""};
+    for (int i = 0; i < race.numberOfLaps; i++)
+    {
+        race.currentLap++;
+        updateRaceCar(raceCar1);
+        updateRaceCar(raceCar2);
+        updateFirstPlace(&race, raceCar1, raceCar2);
+        printFirstPlaceAfterLap(race);
+    }
+    printCongratulation(race);
+}
+int main()
+{
+    // races two racers Jimmy and Tom
     srand(time(0));
-
+    struct RaceCar raceCar1 = {"Mehedi", "Orange", 0};
+    struct RaceCar raceCar2 = {"Mahe", "Yellow", 0};
     printIntro();
     printCountDown();
-
-    return 0;
-}
+    startRace(&raceCar1, &raceCar2);
+};
--- test.c
+++ test.c
@@ -1,14 +1,95 @@
-#include<stdio.h>
+// Inc. Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-void f(int *p) {
-  int b = 2;
-  p = &b;
-  printf("%d ", *p);
+// Structures Sect
+struct Race {
+    int numberOfLaps;
+    int currentLap;
+    char firstPlaceDriverName[60];
+    char firstPlaceRaceCarColor[30];
+};
+
+struct RaceCar {
+    char driverName[60];
+    char raceCarColor[30];
+    int totalLapTime;
+};
+// Functions Prototypes Sect
+// Print Functions Sect
+void printIntro() {
+    printf("Welcome to our main event digital race fans!\nI hope everybody has their snacks because we are about to begin!");
 }
 
-int main() {
-  int a = 1;
-  int *p = &a;
-  f(p);
-  printf("%d ", *p);
+void printCountDown() {
+    printf("Racer's Ready! In...\n");
+    for (int i = 5; i > 0; i--)
+    {
+        printf("%i\n", i);
+    }
+    printf("Race!\n");
+};
+
+void printFirstPlaceAfterLap(struct Race race) {
+    printf("After lap no. %d\n", race.currentLap);
+    printf("First Place -> %s in the %s race car!", race.firstPlaceDriverName, race.firstPlaceRaceCarColor);
 }
+
+void printCongratulation(struct Race race) {
+    printf("Let's all congratulate %s in the %s race car for the extroadinary thrilling event.\n", race.firstPlaceDriverName, race.firstPlaceRaceCarColor);
+    printf("It truly was an amazing race and thank you for attending this experience!");
+}
+// Logic Functions Sect
+int calculateTimeToCompleteLap () {
+    int speed = (rand() % 3) + 1;
+    int acceleration = (rand() % 3) + 1;
+    int nerves = (rand() % 3) + 1;
+
+    return speed + acceleration + nerves;
+    
+}
+
+void updateRaceCar(struct RaceCar* raceCar) {
+    raceCar->totalLapTime += calculateTimeToCompleteLap();
+};
+
+void updateFirstPlace(struct Race *race, struct RaceCar *raceCar1, struct RaceCar *raceCar2) {
+    if (raceCar1->totalLapTime <= raceCar2->totalLapTime)
+    {
+        strcpy(race->firstPlaceDriverName, raceCar1->driverName);
+        strcpy(race->firstPlaceRaceCarColor, raceCar1->raceCarColor);
+    }
+    else
+    {
+        strcpy(race->firstPlaceDriverName, raceCar2->driverName);
+        strcpy(race->firstPlaceRaceCarColor, raceCar2->raceCarColor);
+    }
+};
+
+void startRace(struct RaceCar* racecar1, struct RaceCar* racecar2) {
+    struct Race race = {5, 1, "", ""};
+
+    for (int i = 0; i <= race.numberOfLaps ; i++) {
+        race.currentLap++;
+        updateRaceCar(racecar1);
+        updateRaceCar(racecar2);
+
+        updateFirstPlace(&race, racecar1, racecar2);
+        printFirstPlaceAfterLap(race);
+    }
+    printCongratulation(race);
+}
+
+// Main Script
+int main()
+{
+    // races two racers 
+    srand(time(0));
+    struct RaceCar raceCar1 = {"Mehedi", "Orange", 0};
+    struct RaceCar raceCar2 = {"Mahe", "Yellow", 0};
+    printIntro();
+    printCountDown();
+    startRace(&raceCar1, &raceCar2);
+};
